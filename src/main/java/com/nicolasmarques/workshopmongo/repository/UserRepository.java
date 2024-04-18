5f39d0e4784ee01af5a17063cc2966d8a5092efd
Nicolas <nicolas.marsil02@gmail.com>
2024-04-18 14:21:25 -0300

    feat: Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/nicolasmarques/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nicolasmarques.workshopmongo.repository;
+
+import com.nicolasmarques.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
