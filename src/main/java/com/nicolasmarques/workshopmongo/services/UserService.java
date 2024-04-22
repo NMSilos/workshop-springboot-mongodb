188d7b225ca6df9c3134fa918e9760bcf8f1b103
Nicolas <nicolas.marsil02@gmail.com>
2024-04-22 10:30:22 -0300

    Obtendo um usuário por id

--- src/main/java/com/nicolasmarques/workshopmongo/services/UserService.java
+++ src/main/java/com/nicolasmarques/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.nicolasmarques.workshopmongo.services;
 
 import com.nicolasmarques.workshopmongo.domain.User;
 import com.nicolasmarques.workshopmongo.repository.UserRepository;
+import com.sun.jdi.ObjectCollectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectCollectedException("Objeto não encontrado"));
     }
 
 }
