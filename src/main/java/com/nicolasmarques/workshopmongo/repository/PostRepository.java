dd2519295fd74a413197a828af2c39734e77a78d
Nicolas <nicolas.marsil02@gmail.com>
2024-04-22 13:26:35 -0300

    feat: criando entity Post com User aninhado

---
+++ src/main/java/com/nicolasmarques/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.nicolasmarques.workshopmongo.repository;
+
+import com.nicolasmarques.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
